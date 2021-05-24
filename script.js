02ac51f7156dd3a7fa7e8180ff956b47d8d33326
ashishxcode <ashish.patel0903@gmail.com>
2021-05-24 11:54:19 +0530

    code clean up

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let body = document.querySelector("#body");
-let color1 = document.querySelector("#color1");
-let color2 = document.querySelector("#color2");
-let rotateGradient = document.querySelector("#rotateGradient");
+let body = document.querySelector(".body");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
 let displayValue1 = document.querySelectorAll(".color1Value");
 let displayValue2 = document.querySelectorAll(".color2Value");
 
-function createSnippet(colorCode1, colorCode2) {
+const createSnippet = (colorCode1, colorCode2) => {
   displayValue1.forEach((element) => {
     element.textContent = colorCode1;
   });
@@ -14,17 +13,17 @@ function createSnippet(colorCode1, colorCode2) {
     element.textContent = colorCode2;
     console.log(element.textContent);
   });
-}
+};
 
-function updateGradient() {
+const updateGradient = () => {
   body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
   createSnippet(color1.value, color2.value);
-}
+};
 
-function generateRandomGradient() {
+const generateRandomGradient = () => {
   color1.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
   color2.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
   updateGradient();
-}
+};
 
 updateGradient();
